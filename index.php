6e0022ad67aa93aa52d7b3c50b1787d905a56e85
Arjun19ms <143370928+Arjun19ms@users.noreply.github.com>
2023-08-28 09:41:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+
+<?php
+    echo "welcome";
+
+    $a = 123;
+   $b = 456;
+   $x = $a + $b;
+
+   $abc= true;
+   $xyz=
+
+
+   //constant
+   define('greetings','good morning');
+   echo greetings;
+
+   if ($abc) {
+    # code...
+    $xyz=12345;
+   } else {
+    # code...
+    $xyz="12345";
+   }
+
+
+    $t = date("H");
+    if($t <18){
+        echo "good evening";
+    } else{
+        echo"good night";
+    }
+   
+    //Loop
+    for($k = 0; $k<100; $k++ ) {
+        console.log("value of k: " + $k);
+    }
+    
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
